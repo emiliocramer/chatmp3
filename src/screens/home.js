a87140841227ecc84f769aa6890ff60d7b470c0f
emiliocramer <emilio.cramer101@gmail.com>
2024-04-18 13:05:13 -0700

    change styling to be more unified

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,8 +1,10 @@
 import {SearchBar} from "../components/searchbar/searchbar";
+
 import './home.css';
-import {Emailbar} from "../components/emailbar/emailbar";
 
 export const HomeScreen = () => {
+
+
     return (
         <div className={"home-container"}>
             <h1>ChatMp3</h1>
